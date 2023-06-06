89e53f058a673515585785addbaa2c7684d43597
Mohammad Arif Ishtiaq <49705447+MD-ARIF-073@users.noreply.github.com>
2023-06-06 09:28:58 +0600

    Add files via upload

---
+++ HMC.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hmc;
+
+/**
+ *
+ * @author DELL
+ */
+public class HMC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        hms first = new hms();
+        first.setVisible(true);
+    }
+    
+}
